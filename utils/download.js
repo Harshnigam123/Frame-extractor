652fb0754fe9404cc59b6f7c93f825b22331ac40
Harsh Nigam <harshnigam123@gmail.com>
2026-02-25 00:23:18 +0530

    remove no-cookies flag

--- utils/download.js
+++ utils/download.js
@@ -11,24 +11,23 @@ console.log("=== COOKIE DEBUG END ===");
 
 async function downloadVideo(url, outputPath) {
   try {
-    await ytDlp(url, {
-  format: "bestvideo[height<=720]+bestaudio/best[height<=720]",
+   await ytDlp(url, {
+  format: 'bestvideo[height<=720]+bestaudio/best[height<=720]',
   output: outputPath,
-  cookies: "/etc/secrets/cookies.txt",
-noCookies: true,
+
+  cookies: '/etc/secrets/cookies.txt',
   noPlaylist: true,
 
   addHeader: [
-    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-    "Accept-Language: en-US,en;q=0.9",
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+    'Accept-Language: en-US,en;q=0.9'
   ],
 
-  extractorArgs: "youtube:player_client=android",
+  extractorArgs: 'youtube:player_client=android',
   geoBypass: true,
-
   sleepRequests: 5,
   retries: 3,
-  fragmentRetries: 3,
+  fragmentRetries: 3
 });
 
     console.log("Download completed");
