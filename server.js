e8da6d6e9e86f9a75938bc06ee4aa3015b6f59ac
Harsh Nigam <harshnigam123@gmail.com>
2026-02-25 22:48:21 +0530

    added ffmpeg for railway

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const extractionsRouter = require('./routes/extractions');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
